3f8409d913b82bfc1d9f1a2df2137fd707742374
SNEHILA TIWARI <38729246+SnehaTri@users.noreply.github.com>
2023-07-22 09:33:09 +0530

    Create 13_best_pair.py

---
+++ medium/13_best_pair.py
@@ -0,0 +1,14 @@
+#to find best pair ina list with maximum length and all numbers from 0 to 5 are present
+
+def best_pair(lst):
+    max_length = 0
+    for i in range(len(lst)-2):
+        for j in range(i+2, len(lst)):
+            str = lst[i] + lst[j]
+            length = len(str)
+            if '0' in str and '1' in str and '2' in str and '3' in str and '4' in str and '5' in str:
+                if length > max_length:
+                    max_length = length
+    return str
+
+print(best_pair(['103244', '03122222', '50111', '12345']))
