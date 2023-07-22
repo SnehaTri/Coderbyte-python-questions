448313eba212a609088744038943d21659b46d74
SNEHILA TIWARI <38729246+SnehaTri@users.noreply.github.com>
2023-07-22 09:18:04 +0530

    Update 11_sum_without_carry.py

--- medium/11_sum_without_carry.py
+++ medium/11_sum_without_carry.py
@@ -1,1 +1,11 @@
+#Sum digits of equal length numbers without carrying forward
 
+def sum_without_cary(num1, num2):
+    s = ''
+    for x, y in zip(str(num1), str(num2)):
+        sum = (int(x) + int(y))%10
+        s += str(sum)
+
+    return int(s)
+
+print(sum_without_cary(359, 354))
