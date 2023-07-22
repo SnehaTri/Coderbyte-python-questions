0050f7205e791d80176fad7642ca3a6ceecf5d25
SNEHILA TIWARI <38729246+SnehaTri@users.noreply.github.com>
2023-07-22 09:48:56 +0530

    Update 13_best_pair.py

--- medium/13_best_pair.py
+++ medium/13_best_pair.py
@@ -2,6 +2,7 @@
 
 def best_pair(lst):
     max_length = 0
+
     for i in range(len(lst)-2):
         for j in range(i+2, len(lst)):
             str = lst[i] + lst[j]
@@ -9,6 +10,7 @@ def best_pair(lst):
             if '0' in str and '1' in str and '2' in str and '3' in str and '4' in str and '5' in str:
                 if length > max_length:
                     max_length = length
-    return str
+                    max_str = str
+    return max_str
 
-print(best_pair(['103244', '03122222', '50111', '12345']))
+print(best_pair(['103244', '01222222', '50111', '12345']))
