0d62a04f4927c0ee0c9e64de8052eb882d263930
SNEHILA TIWARI <38729246+SnehaTri@users.noreply.github.com>
2023-07-27 18:50:32 +0530

    Create 14_rotate_mat_by_90.py

---
+++ medium/14_rotate_mat_by_90.py
@@ -0,0 +1,11 @@
+def rotate(matrix):
+    n = len(matrix)
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i+1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Reverse each row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
+    return matrix
